c864d121bab6e04207553348d0831790cb05cf00
Johan Torres <keivertorres12@gmail.com>
2021-07-15 20:01:10 -0500

    error catcher from search added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,11 +10,14 @@ import { Label } from 'ng2-charts';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  errorMessage: String = '';
+  //search bar input
+  inputText: any;
+  //Used to get and manipulate fetched data
   profiles: any[] = [];
   loadedProfiles: any[] = [];
+  //Chart info declare empty so then i can pass it the fetched data
   chartInfo: any;
-  inputText: any;
-
   barChartData: ChartDataSets[] = [{ data: [], label: 'Followers chart' }];
   barChartLabels: Label[] = [];
   barChartOptions = {
@@ -27,16 +30,20 @@ export class HomeComponent {
   constructor(private _http: HttpService, private router: Router) {}
 
   searchProfiles() {
-    if (this.inputText.length > 4 && this.inputText != 'doublevpartners') {
+    if (this.inputText.length > 3 && this.inputText != 'doublevpartners') {
+      //if the search is correct, then there isn't error message'
+      this.errorMessage = '';
       this._http.getApiData(this.inputText).subscribe((data: any) => {
         this.profiles = data['items'];
         this.loadedProfiles = [];
+        // we´re going to get the followers from for each profile loaded from the search
         this.profiles.forEach((profile: any, index: number) => {
           this._http.getProfileData(profile.url).subscribe((data: any) => {
             this.loadedProfiles.push({
               name: data.login,
               follow: data.followers,
             });
+            // if the array "loadedProfiles" contains the same lenght as profiles we´re going to set that data to the chart
             if (this.loadedProfiles.length == this.profiles.length) {
               let followers = this.loadedProfiles.map(
                 (profileData, index) => this.loadedProfiles[index].follow
@@ -52,6 +59,11 @@ export class HomeComponent {
           });
         });
       });
+    } else if (this.inputText.length < 4) {
+      this.errorMessage =
+        'El texto de la búsqueda debe contener un mínimo de 4 caracteres.';
+    } else {
+      this.errorMessage = 'La búsqueda “doublevpartners” no está permitida.';
     }
   }
 
